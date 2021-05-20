751b15bc66245445567626114b8b47ba2721d40f
Manali Mathur <a.manalimathur@gmail.com>
2021-05-20 19:11:40 +0530

    Edit Recipe JSX and CSS

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import RecipeList from "./RecipeList";
 import "../css/App.css"
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import RecipeEdit from "./RecipeEdit";
 
 export const RecipeContext = React.createContext()
 const LOCAL_STORAGE_KEY = "cookingWithReact.recipes";
@@ -45,6 +46,7 @@ function App() {
   return (
     <RecipeContext.Provider value={recipeContextValue}>
       <RecipeList recipes={recipes} />
+      <RecipeEdit />
     </RecipeContext.Provider>
   )
 }
