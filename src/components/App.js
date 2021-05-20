b24747fc5f126657e19bb792782c567a29468d93
Manali Mathur <a.manalimathur@gmail.com>
2021-05-20 20:39:19 +0530

    Add and delete ingredient & minor code changes

--- src/components/App.js
+++ src/components/App.js
@@ -42,19 +42,23 @@ function App() {
   function handleRecipeAdd() {
     const newRecipe = {
       id: uuidv4(),
-      name: "New",
+      name: "",
       servings: 1,
-      cookTime: "1:00",
-      instructions: "instr.",
+      cookTime: "",
+      instructions: "",
       ingredients: [
-        { id: uuidv4(), name: "Name", amount: "1 Tbs" }
+        { id: uuidv4(), name: "", amount: "" }
       ]
     }
 
+    setSelectedRecipeId(newRecipe.id)
     setRecipes([...recipes, newRecipe])
   }
 
   function handleRecipeDelete(id) {
+    if (selectedRecipeId != null && selectedRecipeId === id) {
+      setSelectedRecipeId(undefined)
+    }
     setRecipes(recipes.filter(recipe => recipe.id !== id))
   }
 
